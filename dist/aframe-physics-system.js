5ba0ff9b845ed949c8e1ed26d804d7881fcd87a7
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-10-01 14:34:19 +0100

    Updated builds

--- dist/aframe-physics-system.js
+++ dist/aframe-physics-system.js
@@ -34,6 +34,15 @@ var CANNON = require('cannon-es');
 CANNON.shape2mesh = function(body){
     var obj = new THREE.Object3D();
 
+    function createBufferGeometry(vertices, faces) {
+
+      var geometry = new THREE.BufferGeometry();
+      geometry.setFromPoints(vertices);
+      geometry.setIndex(faces);
+      geometry.computeBoundingSphere();
+      return geometry;
+    }
+
     for (var l = 0; l < body.shapes.length; l++) {
         var shape = body.shapes[l];
 
@@ -74,14 +83,15 @@ CANNON.shape2mesh = function(body){
             break;
 
         case CANNON.Shape.types.CONVEXPOLYHEDRON:
-            var geo = new THREE.Geometry();
-
+            
             // Add vertices
+            var vertices = []
             for (var i = 0; i < shape.vertices.length; i++) {
                 var v = shape.vertices[i];
-                geo.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
+                vertices.push(new THREE.Vector3(v.x, v.y, v.z));
             }
 
+            var faces = []
             for(var i=0; i < shape.faces.length; i++){
                 var face = shape.faces[i];
 
@@ -90,20 +100,21 @@ CANNON.shape2mesh = function(body){
                 for (var j = 1; j < face.length - 1; j++) {
                     var b = face[j];
                     var c = face[j + 1];
-                    geo.faces.push(new THREE.Face3(a, b, c));
+                    faces.push(a, b, c);
                 }
             }
-            geo.computeBoundingSphere();
-            geo.computeFaceNormals();
+
+            var geo = createBufferGeometry(vertices, faces);
             mesh = new THREE.Mesh( geo, this.currentMaterial );
             break;
 
         case CANNON.Shape.types.HEIGHTFIELD:
-            var geometry = new THREE.Geometry();
 
             var v0 = new CANNON.Vec3();
             var v1 = new CANNON.Vec3();
             var v2 = new CANNON.Vec3();
+            var vertices = [];
+            var faces = [];
             for (var xi = 0; xi < shape.data.length - 1; xi++) {
                 for (var yi = 0; yi < shape.data[xi].length - 1; yi++) {
                     for (var k = 0; k < 2; k++) {
@@ -114,39 +125,39 @@ CANNON.shape2mesh = function(body){
                         v0.vadd(shape.pillarOffset, v0);
                         v1.vadd(shape.pillarOffset, v1);
                         v2.vadd(shape.pillarOffset, v2);
-                        geometry.vertices.push(
+                        vertices.push(
                             new THREE.Vector3(v0.x, v0.y, v0.z),
                             new THREE.Vector3(v1.x, v1.y, v1.z),
                             new THREE.Vector3(v2.x, v2.y, v2.z)
                         );
-                        var i = geometry.vertices.length - 3;
-                        geometry.faces.push(new THREE.Face3(i, i+1, i+2));
+                        var i = vertices.length - 3;
+                        faces.push(i, i+1, i+2);
                     }
                 }
             }
-            geometry.computeBoundingSphere();
-            geometry.computeFaceNormals();
+            var geometry = createBufferGeometry(vertices, faces);
             mesh = new THREE.Mesh(geometry, this.currentMaterial);
             break;
 
         case CANNON.Shape.types.TRIMESH:
-            var geometry = new THREE.Geometry();
+            var geometry = new THREE.BufferGeometry();
 
             var v0 = new CANNON.Vec3();
             var v1 = new CANNON.Vec3();
             var v2 = new CANNON.Vec3();
+            var vertices = [];
+            var faces = [];
             for (var i = 0; i < shape.indices.length / 3; i++) {
                 shape.getTriangleVertices(i, v0, v1, v2);
-                geometry.vertices.push(
+                vertices.push(
                     new THREE.Vector3(v0.x, v0.y, v0.z),
                     new THREE.Vector3(v1.x, v1.y, v1.z),
                     new THREE.Vector3(v2.x, v2.y, v2.z)
                 );
-                var j = geometry.vertices.length - 3;
-                geometry.faces.push(new THREE.Face3(j, j+1, j+2));
+                var j = vertices.length - 3;
+                faces.push(j, j+1, j+2);
             }
-            geometry.computeBoundingSphere();
-            geometry.computeFaceNormals();
+            var geometry = createBufferGeometry(vertices, faces);
             mesh = new THREE.Mesh(geometry, this.currentMaterial);
             break;
 
