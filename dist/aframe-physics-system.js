dc51eaab94e9a692c34536c8fd4d7a9576995123
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-01-04 13:22:18 +0000

    Fix THREE.js deprecations in ammo-debug-drawer

--- dist/aframe-physics-system.js
+++ dist/aframe-physics-system.js
@@ -519,13 +519,13 @@ THREE.AmmoDebugDrawer = function(scene, world, options) {
   var vertices = new Float32Array(maxBufferSize * 3);
   var colors = new Float32Array(maxBufferSize * 3);
 
-  this.geometry.addAttribute("position", new THREE.BufferAttribute(vertices, 3).setDynamic(true));
-  this.geometry.addAttribute("color", new THREE.BufferAttribute(colors, 3).setDynamic(true));
+  this.geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3).setUsage(THREE.DynamicDrawUsage));
+  this.geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3).setUsage(THREE.DynamicDrawUsage));
 
   this.index = 0;
 
   var material = new THREE.LineBasicMaterial({
-    vertexColors: THREE.VertexColors,
+    vertexColors: true,
     depthTest: !drawOnTop
   });
 
